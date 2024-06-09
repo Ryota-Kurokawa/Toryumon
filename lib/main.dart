2bbaf61ef34bf26b99e55435b145672b651f022c
ryota <kurokawa.ryota.1124@gmail.com>
2024-06-09 23:04:08 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -20,32 +24,58 @@ class MyApp extends StatelessWidget {
   }
 }
 
-// Hookを使うためにはHookWidgetを継承する
 class MyHomePage extends HookWidget {
   const MyHomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final counter = useState(0);
+    final TextEditingController _textcontroller = useTextEditingController();
+    final db = FirebaseFirestore.instance;
+    final docRef = db.collection('users').doc();
+    final lists = useState([]);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flutter Demo'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('ボタンを押した回数は'),
-            Text(counter.value.toString()),
-            ElevatedButton(
-              onPressed: () {
-                counter.value++;
-              },
-              child: const Text("increment"),
-            )
-          ],
-        ),
-      ),
+          child: Column(
+        children: [
+          const Text('Enter your name'),
+          TextField(
+            controller: _textcontroller,
+            decoration: const InputDecoration(
+              hintText: 'Enter your name',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              debugPrint(_textcontroller.text);
+              db.collection('users').doc().set({'name': _textcontroller.text});
+            },
+            child: const Text('Submit'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              db.collection('users').get().then((QuerySnapshot querySnapshot) {
+                querySnapshot.docs.forEach((doc) {
+                  lists.value.add(doc['name']);
+                  debugPrint(doc['name']);
+                });
+              });
+            },
+            child: const Text("fetch data"),
+          ),
+          Divider(),
+          ListView.builder(
+            itemCount: lists.value.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(lists.value[index]),
+              );
+            },
+          )
+        ],
+      )),
     );
   }
 }
